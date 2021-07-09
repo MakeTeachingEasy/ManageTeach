7a71f807571db94b872d7ed940a91bb5b0acb394
Tanmay Agrawal <tanmayagrawal2004@gmail.com>
2021-07-09 22:06:24 +0530

    Vulnerabilities fixed and Bug Fixes for Time in Attendance Log Added

--- usersOnly/views.py
+++ usersOnly/views.py
@@ -13,42 +13,49 @@ def attendenceView(response):
         return redirect("/login")
 
     if response.method=='POST':
-        if not response.POST.get("date"):
+        if (not int(response.POST.get("time")) in range(2,7)) or \
+             (not response.POST.get("date")):
             return render(response, "usersOnly/attendence.html", {
                 "user": response.user, 
                 "students_class_wise": students_class_wise,
-                "date_not_filled": True,
+                "illegal_entry": True,
             })
             
         else:
             print(response.POST)
             date= response.POST.get("date")
             time= datetime.time(int(response.POST.get("time"))+12, 0, 0)
+            log_array=[]
             for student in Student.objects.all():
                 if response.POST.get(f"c{student.id}") == "present":
                     log= Log()
                     log.logIt(response.user, student, date, time, True, AbsenceType.objects.get(type="Does Not Apply"))
                     log.save()
-                if response.POST.get(f"c{student.id}") == "informed":
+                    log_array.append(log)
+                elif response.POST.get(f"c{student.id}") == "informed":
                     log= Log()
                     log.logIt(response.user, student, date, time, False, AbsenceType.objects.get(type="Informed"))
                     log.save()
-                if response.POST.get(f"c{student.id}") == "uninformed":
+                    log_array.append(log)
+                elif response.POST.get(f"c{student.id}") == "uninformed":
                     log=Log()
                     log.logIt(response.user, student, date, time, False, AbsenceType.objects.get(type="Uninformed"))
                     log.save()
-            return HttpResponseRedirect("/")
+                    log_array.append(log)
+            return myLogView(response, True, log_array)
             
     return render(response, "usersOnly/attendence.html", {
         "user": response.user, 
         "students_class_wise": students_class_wise,
-        "date_not_filled": False,
+        "illegal_entry": False,
         })
-def myLogView(response):
+def myLogView(response, submitted=False, log_array=[]):
     if response.user.is_authenticated:
         all_logs= Log.objects.filter(tutor=response.user)
         return render(response, "usersOnly/myLogs.html", {
             "logs": all_logs.order_by("date").reverse(),
+            "submitted": submitted,
+            "log_array": log_array,
         })
     else:
         return redirect('/login')
