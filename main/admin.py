dc902307fdc66ef936aa04a6980e4778494907c4
Tanmay Agrawal <tanmayagrawal2004@gmail.com>
2021-07-22 01:18:53 +0530

    Admin Portal Made Professional

--- main/admin.py
+++ main/admin.py
@@ -3,6 +3,9 @@ from .models import AbsenceType, Log, Student, Grade, Board
 from usersOnly.models import TutorSlot
 # Register your models here.
 
+admin.site.site_title="TLP Admin"
+admin.site.site_header= "The Lockdown Project Admin Portal"
+
 admin.site.register(Grade)
 admin.site.register(Student)
 admin.site.register(Board)
